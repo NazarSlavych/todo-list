c7723d3075d1cb3a87dcad64430d153923cd1f72
Nazar Slavych <slavuch232355@gmail.com>
2025-03-12 00:10:15 +0200

    Todo website complete

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Tag, Task
 
-# Register your models here.
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'content', 'created_at', 'deadline', 'is_done')
+    list_filter = ('is_done', 'created_at', 'deadline')
+    search_fields = ('content',)
+    ordering = ('-created_at',)
+    filter_horizontal = ('tags',)
+
+
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Task, TaskAdmin)
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,69 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, View
+from django.shortcuts import get_object_or_404, redirect
+from .models import Task, Tag
 
-# Create your views here.
+
+class TaskListView(ListView):
+    model = Task
+    template_name = 'todo/task_list.html'
+
+    def get_queryset(self):
+        return Task.objects.all().order_by('is_done', '-created_at')
+
+
+class TagsListView(ListView):
+    model = Tag
+    template_name = 'todo/tag_list.html'
+
+    def get_queryset(self):
+        return Tag.objects.all().order_by('name')
+
+
+class TaskCreateView(CreateView):
+    model = Task
+    fields = "__all__"
+    template_name = 'todo/task_form.html'
+    success_url = reverse_lazy('todo:task_list')
+
+
+class TagsCreateView(CreateView):
+    model = Tag
+    fields = "__all__"
+    template_name = 'todo/tag_form.html'
+    success_url = reverse_lazy('todo:tags_list')
+
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    fields = "__all__"
+    template_name = 'todo/task_form.html'
+    success_url = reverse_lazy('todo:task_list')
+
+
+class TagsUpdateView(UpdateView):
+    model = Tag
+    fields = "__all__"
+    template_name = 'todo/tag_form.html'
+    success_url = reverse_lazy('todo:tags_list')
+
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    template_name = 'todo/task_confirm_delete.html'
+    success_url = reverse_lazy('todo:task_list')
+
+
+class TagsDeleteView(DeleteView):
+    model = Tag
+    template_name = 'todo/tag_confirm_delete.html'
+    success_url = reverse_lazy('todo:tags_list')
+
+
+class TaskToggleStatusView(View):
+    def post(self, request, pk):
+        task = get_object_or_404(Task, pk=pk)
+        task.is_done = not task.is_done
+        task.save()
+        return redirect('todo:task_list')
+
